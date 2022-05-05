bc04d4ef145003ace9fd913a7d4239218dcbba1c
Vladimir Kozhevnikov <kv.vent@yandex.ru>
2022-05-05 21:27:34 +0300

    project 6 part 3

--- scripts/index.js
+++ scripts/index.js
@@ -24,75 +24,45 @@ const modalCloseBtnImage = document.querySelector(".popup__close-picture");
 const listCard = document.querySelector(".elements__groups");
 const templateCard = document.querySelector(".template");
 
-modalEditBtn.addEventListener("click", () => openPopup(modalWindow));
-modalPlusBtn.addEventListener("click", () => openPopup(modalNewWindow));
+const escClosed = "Escape";
 
-function openPopup(popup) {
-  popup.classList.add("popup_opened");
-  document.addEventListener("keydown", doSomething);
-  resetButton();
-  resetInput();
-}
-
-function resetButton() {
-  const formResetButton = formElementCard.querySelector(".popup__button_valid-card");
-  formResetButton.classList.add("button_disabled");
-}
-
-function resetInput() {
-  const formResetInput = formElementCard.querySelectorAll(".popup__input");
-  console.log(formResetInput);
-  Array.from(formResetInput).forEach((formResetInput) => {
-  formResetInput.removeAttribute('required');
-});
-}
-
-
-function closePopup(popupElement) {
-  popupElement.classList.remove("popup_opened");
-  document.removeEventListener("keydown", doSomething);
-}
-
-modalCloseBtn.addEventListener("click", function () {
-  closePopup(modalWindow);
-});
-modalCloseNewBtn.addEventListener("click", function () {
-  closePopup(modalNewWindow);
-});
-modalCloseBtnImage.addEventListener("click", function () {
-  closePopup(modalImage);
-});
-
-//закрытие окна через ESC
-function doSomething(evt) {
-  const escClosed = "Escape";
-  const popupActive = document.querySelector(".popup_opened");
-  if (evt.key === escClosed) {
-    closePopup(popupActive);
-  }
-}
-
-//закрытие окна через overlay
-document.addEventListener("click", function (event) {
-  if (event.target.classList.contains("popup_opened")) {
-    const popupActive = document.querySelector(".popup_opened");
-    closePopup(popupActive);
-  }
-});
+const formResetButton = formElementCard.querySelector(".popup__button_valid-card");
 
 // редактирование профайла
 nameInput.value = profileName.textContent;
 jobInput.value = profileStatus.textContent;
 
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
+  document.addEventListener("keydown", handleEscClose);
+  resetInput();
+}
+
+function resetInput() {
+  const formResetInput = formElementCard.querySelectorAll(".popup__input");
+  Array.from(formResetInput).forEach((formResetInput) => {
+  formResetInput.removeAttribute('required');
+});
+}
+
+function closePopup(popupElement) {
+  popupElement.classList.remove("popup_opened");
+  document.removeEventListener("keydown", handleEscClose);
+}
+
+//закрытие окна через ESC
+function handleEscClose(evt) {
+  if (evt.key === escClosed) {
+  closePopup(document.querySelector(".popup_opened"));
+  }
+}
+
 function submitProfileForm(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileStatus.textContent = jobInput.value;
-  const popupActive = document.querySelector(".popup_opened");
-  closePopup(popupActive);
+  closePopup(document.querySelector(".popup_opened"));
 }
-
-formProfileElement.addEventListener("submit", submitProfileForm);
 
 // создание карточек
 function renderCard() {
@@ -106,11 +76,11 @@ function СreateСard(card) {
   const name = newCard.querySelector(".elements__text");
   const noticeVector = newCard.querySelector(".elements__vector");
   const removeButton = newCard.querySelector(".elements__delete");
-
+  
   name.textContent = card.name;
   link.src = card.link;
   link.alt = card.name;
-
+  
   removeButton.addEventListener("click", removeElementButton);
   noticeVector.addEventListener("click", likeCardElements);
   link.addEventListener("click", addElementImage);
@@ -152,8 +122,32 @@ formElementCard.addEventListener("submit", function (evt) {
   });
   listCard.prepend(elementCard);
   formElementCard.reset();
-  const popupActive = document.querySelector(".popup_opened");
-  closePopup(popupActive);
+  closePopup(document.querySelector(".popup_opened"));
 });
 
 renderCard();
+
+//закрытие окна через overlay
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("popup_opened")) {
+     closePopup(document.querySelector(".popup_opened"));
+  }
+});
+
+formProfileElement.addEventListener("submit", submitProfileForm);
+modalEditBtn.addEventListener("click", () => openPopup(modalWindow));
+modalPlusBtn.addEventListener("click", function() { 
+  formResetButton.classList.add("button_disabled");
+  openPopup(modalNewWindow);
+});
+
+modalCloseBtn.addEventListener("click", function () {
+  closePopup(modalWindow);
+});
+modalCloseNewBtn.addEventListener("click", function () {
+  formResetButton.classList.remove("button_disabled");
+  closePopup(modalNewWindow);
+});
+modalCloseBtnImage.addEventListener("click", function () {
+  closePopup(modalImage);
+});
