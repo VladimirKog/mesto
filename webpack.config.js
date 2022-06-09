0c10a4177a95512d8d1b96cbc1519493b2bb3b76
Vladimir Kozhevnikov <kv.vent@yandex.ru>
2022-06-09 08:24:27 +0300

    Коммит ПР8 1rev

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: { main: "./src/scripts/index.js" },
+  entry: { main: "./src/pages/index.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
@@ -13,9 +13,11 @@ module.exports = {
   mode: "development",
   devtool: "source-map",
   devServer: {
-    static: path.resolve(__dirname, "./dist"), // путь, куда "смотрит" режим разработчика
+    static: {
+      directory: path.resolve(__dirname, "./dist")
+    }, // путь, куда "смотрит" режим разработчика
     compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    port: 8000, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
     open: true, // сайт будет открываться сам при запуске npm run dev
   },
   module: {
@@ -25,13 +27,13 @@ module.exports = {
         // при обработке этих файлов нужно использовать babel-loader
         use: "babel-loader",
         // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: "/node_modules/",
+        exclude: "/node_modules/"
       },
       // добавили правило для обработки файлов
       {
         // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
+        type: "asset/resource"
       },
       // правила для обработки js, html и других файлов
 
