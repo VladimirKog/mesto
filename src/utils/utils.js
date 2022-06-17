018b4cf11d922db5384ba846d820ba57664635b9
Vladimir Kozhevnikov <kv.vent@yandex.ru>
2022-06-17 21:39:51 +0300

    Коммит ПР8 3rev

---
+++ src/utils/utils.js
@@ -0,0 +1,8 @@
+export const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'button_disabled',
+  inputErrorClass: 'input_disabled',
+  errorClass: 'popup__error'
+};
